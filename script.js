a92237c10793ebd40567c13fbff5e2b432675b78
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2023-09-24 15:11:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const slider = document.querySelector('.slider');
-let counter = 1;
-const sliderWidth = slider.clientWidth;
 
-setInterval(function() {
-  slider.style.transition = 'transform 1s ease-in-out';
-  slider.style.transform = 'translateX(' + (-sliderWidth * counter) + 'px)';
-  counter++;
-
-  if (counter >= slider.children.length) {
-    counter = 0;
-    setTimeout(function() {
-      slider.style.transition = 'none';
-      slider.style.transform = 'translateX(' + (-sliderWidth * counter) + 'px)';
-    }, 1000);
-  }
-}, 3000);
