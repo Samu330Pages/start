88d4d691c3322555da3bc49e0a38d3086cf0492d
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2023-09-24 15:25:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+window.addEventListener('scroll', function() {
+  var logo = document.getElementById('logo');
+  var scrollPosition = window.pageYOffset;
+  
+  if (scrollPosition > 100) {
+    logo.style.opacity = 0;
+  } else {
+    logo.style.opacity = 1;
+  }
+});
