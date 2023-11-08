f6149a0a92f8af39406581f14feb46a0a62c0acd
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2023-11-08 00:37:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
   var formulario = document.getElementById("formulario");
   var listaEspera = document.getElementById("listaEspera");
   var btnsPuesto = document.querySelectorAll(".btn-puesto");
-  
+
   formulario.addEventListener("submit", function(e) {
     e.preventDefault();
-    
+
     var nombre = document.getElementById("nombre").value;
     var sexo = document.getElementById("sexo").value;
     var puesto = "";
@@ -15,54 +15,51 @@ document.addEventListener("DOMContentLoaded", function() {
         puesto = btn.textContent;
       }
     });
-    
+
     if (nombre && sexo && puesto) {
       var nuevoElemento = document.createElement("li");
       nuevoElemento.innerHTML = nombre + " - " + sexo + " - " + puesto;
-      
+
       var btnBorrar = document.createElement("button");
       btnBorrar.textContent = "Borrar";
       btnBorrar.addEventListener("click", function() {
         this.parentElement.remove();
         habilitarBotones();
       });
-      
+
       nuevoElemento.appendChild(btnBorrar);
-      
+
       listaEspera.appendChild(nuevoElemento);
-      
+
       formulario.reset();
-      
+
       restaurarColores();
     }
   });
-  
+
   btnsPuesto.forEach(function(btn) {
     btn.addEventListener("click", function() {
       if (!this.classList.contains("active")) {
         btnsPuesto.forEach(function(btn) {
           btn.classList.remove("active");
+          btn.style.backgroundColor = "";
+          btn.style.color = "";
         });
-        
+
         this.classList.add("active");
-        
-        cambiarColor(this);
+        this.style.backgroundColor = "blue";
+        this.style.color = "white";
       }
     });
   });
-  
-  function cambiarColor(btn) {
-    btn.style.backgroundColor = "green";
-    btn.style.color = "white";
-  }
-  
+
   function restaurarColores() {
     btnsPuesto.forEach(function(btn) {
       btn.style.backgroundColor = "";
       btn.style.color = "";
     });
   }
-  
+
   function habilitarBotones() {
     btnsPuesto.forEach(function(btn) {
       btn.removeAttribute("disabled");
