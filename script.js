3fe52ed16e1eda37b5482ae2edf24c0ef456fa12
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2023-11-08 00:43:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var nombre = document.getElementById("nombre").value;
     var sexo = document.getElementById("sexo").value;
     var puesto = "";
-    
+
     btnsPuesto.forEach(function(btn) {
       if (btn.classList.contains("active")) {
         puesto = btn.textContent;
@@ -23,17 +23,31 @@ document.addEventListener("DOMContentLoaded", function() {
       var btnBorrar = document.createElement("button");
       btnBorrar.textContent = "Borrar";
       btnBorrar.addEventListener("click", function() {
+        clearInterval(temporizador);
         this.parentElement.remove();
         habilitarBotones();
       });
 
       nuevoElemento.appendChild(btnBorrar);
 
+      var tiempo = document.createElement("span");
+      tiempo.textContent = "00:00";
+      nuevoElemento.appendChild(tiempo);
+
       listaEspera.appendChild(nuevoElemento);
 
       formulario.reset();
 
       restaurarColores();
+
+      var tiempoInicio = Date.now();
+      var temporizador = setInterval(function() {
+        var tiempoActual = Date.now();
+        var tiempoTranscurrido = tiempoActual - tiempoInicio;
+        var minutos = Math.floor(tiempoTranscurrido / 60000);
+        var segundos = Math.floor((tiempoTranscurrido % 60000) / 1000);
+        tiempo.textContent = ("0" + minutos).slice(-2) + ":" + ("0" + segundos).slice(-2);
+      }, 1000);
     }
   });
 
