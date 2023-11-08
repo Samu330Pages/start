bf12c0d623a25140d46c28021fb2d2592a527ede
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2023-11-07 23:51:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,85 @@
-//---------- Navbar ----------//
+var tasks = [];
 
-window.addEventListener('scroll', function(){
-    let navbar = document.getElementById("navbar");
-    navbar.classList.toggle('fixed', this.window.scrollY > 0)
-})
+function addTask() {
+  var taskText = document.getElementById('input-task').value;
 
-// Portfolio Gallery
+  var task = {
+    text: taskText,
+    buttons: []
+  };
 
-let filterContainer = document.querySelector(".gallery-filter");
-let galleryItems = document.querySelectorAll(".gallery-item");
+  tasks.push(task);
 
-filterContainer.addEventListener("click", (event) => {
-    if (event.target.classList.contains("filter-item")){
-        // deactivates existing active filter item
-        filterContainer.querySelector(".active").classList.remove("active");
-        // activates new filter item
-        event.target.classList.add("active");
-        let filterValue = event.target.getAttribute("data-filter");
-        galleryItems.forEach((item) => {
-            if (item.classList.contains(filterValue) || filterValue === "all") {
-                item.classList.remove("hide");
-                item.classList.add("show");
-            }
-            else {
-                item.classList.remove("show");
-                item.classList.add("hide");
-            }
-        });
-    }
+  renderTasks();
+}
 
-});
+function renderTasks() {
+  var tasksContainer = document.getElementById('tasks-container');
+  tasksContainer.innerHTML = '';
 
-// Review Carousel
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1200:{
-            items:2
+  tasks.forEach(function(task, index) {
+    var taskEl = document.createElement('div');
+    taskEl.className = 'task';
+
+    var taskInfo = document.createElement('div');
+    taskInfo.className = 'task-info';
+
+    var textEl = document.createElement('span');
+    textEl.textContent = task.text;
+
+    var timeEl = document.createElement('span');
+    timeEl.textContent = 'Hace ' + index + ' segundos'; // Cambiar esto por la lógica para calcular el tiempo
+
+    taskInfo.appendChild(textEl);
+    taskInfo.appendChild(timeEl);
+
+    var actionsEl = document.createElement('div');
+    actionsEl.className = 'task-actions';
+
+    var addButtons = [
+      {text: '1', value: 1},
+      {text: '2', value: 2},
+      {text: '3', value: 3},
+      {text: '4', value: 4},
+    ];
+
+    var updateButtons = [
+      {text: '1', value: 1},
+      {text: '2', value: 2},
+      {text: '3', value: 3},
+      {text: '4', value: 4},
+      {text: 'Eliminar', value: 'delete'},
+    ];
+
+    addButtons.forEach(function(button) {
+      var btnEl = document.createElement('button');
+      btnEl.textContent = button.text;
+      btnEl.addEventListener('click', function() {
+        task.buttons.push(button.value);
+        renderTasks();
+      });
+      actionsEl.appendChild(btnEl);
+    });
+
+    task.buttons.forEach(function(button) {
+      var btnEl = document.createElement('button');
+      btnEl.textContent = button;
+      btnEl.addEventListener('click', function() {
+        if (button === 'delete') {
+          tasks.splice(index, 1);
+        } else {
+          // Lógica para actualizar la tarea con el botón seleccionado
         }
-    }
-})
+        renderTasks();
+      });
+      actionsEl.appendChild(btnEl);
+    });
+
+    taskEl.appendChild(taskInfo);
+    taskEl.appendChild(actionsEl);
+
+    tasksContainer.appendChild(taskEl);
+  });
+}
+
+document.getElementById('add-task-btn').addEventListener('click', addTask);
