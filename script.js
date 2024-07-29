486aa7c472e94431b141aeb7eed785348444477f
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2024-07-29 03:47:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -61,17 +61,19 @@ const previewFrame = document.getElementById('preview-frame');
 const previewTitle = document.getElementById('preview-title');
 const closePreviewButton = document.getElementById('close-preview-button');
 
-slides.forEach((slide) => {
-    slide.addEventListener('click', () => {
-        const imgId = slide.id;
-        const previewHtml = `${imgId}.html`;
-        previewFrame.src = previewHtml;
-        previewTitle.textContent = `Vista previa de ${slide.alt}`;
-        overlay.classList.remove('hidden');
-    });
+const pics = document.querySelectorAll('.image-container img');
+
+pics.forEach((pic) => {
+  pic.addEventListener('click', () => {
+    const imgId = pic.id;
+    const previewHtml = `${imgId}.html`;
+    previewFrame.src = previewHtml;
+    previewTitle.textContent = `Vista previa de ${pic.alt}`;
+    overlay.classList.remove('hidden');
+  });
 });
 
 closePreviewButton.addEventListener('click', () => {
-    overlay.classList.add('hidden');
-    previewFrame.src = '';
+  overlay.classList.add('hidden');
+  previewFrame.src = '';
 });
