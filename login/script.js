5a74d9952bc2793c5ba914f788b3156ef4d625b3
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2025-07-31 12:24:29 -0500

    Update script.js

--- login/script.js
+++ login/script.js
@@ -180,3 +180,17 @@ document.addEventListener('keydown', function(event) {
         )
     }
 });
+document.querySelectorAll('.toggle-password').forEach(icon => {
+  icon.addEventListener('click', () => {
+    const passwordInput = icon.previousElementSibling;
+    if (passwordInput.type === 'password') {
+      passwordInput.type = 'text';
+      icon.classList.remove('fa-eye');
+      icon.classList.add('fa-eye-slash');
+    } else {
+      passwordInput.type = 'password';
+      icon.classList.remove('fa-eye-slash');
+      icon.classList.add('fa-eye');
+    }
+  });
+});
