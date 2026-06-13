3d5cc183cd41ac8bb64e2ff6ce9a1d0c5776535c
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2026-06-13 00:56:39 -0500

    Update master-loader.js

--- games/surf/js/master-loader.js
+++ games/surf/js/master-loader.js
@@ -3,7 +3,7 @@ var scripts = document.getElementsByTagName("script"),
     scriptUrl = scripts[scripts.length - 1].src,
     root = scriptUrl.split("master-loader.js")[0],
     loaders = {
-        unity: "js/unity.js",
+        unity: "unity.js",
         "unity-beta": "unity-beta.js",
         "unity-2020": "unity-2020.js"
     };
