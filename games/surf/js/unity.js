7a59321ecd7a00116e284e7a6d0f4f7ff6bd70bf
Samu330Pages <145924555+Samu330Pages@users.noreply.github.com>
2026-06-13 00:02:08 -0500

    Update unity.js

--- games/surf/js/unity.js
+++ games/surf/js/unity.js
@@ -1,13 +1,14 @@
 "use strict";
 
-// 1. Crear el contenedor oficial en el DOM
 var container = document.createElement("div");
 container.id = "unity-container";
-container.style.width = "100%";
-container.style.height = "100%";
-container.style.position = "absolute";
+container.style.position = "fixed";
 container.style.top = "0";
 container.style.left = "0";
+container.style.width = "100vw";
+container.style.height = "100vh";
+container.style.backgroundColor = "#222428";
+container.style.zIndex = "999999";
 document.body.appendChild(container);
 
 var loaderScript = document.createElement("script");
@@ -25,10 +26,15 @@ loaderScript.onload = function() {
             },
             Module: {
                 backgroundColor: "#222428",
-                preferLowPowerToHighPerformance: true 
+                preferLowPowerToHighPerformance: true,
+                compatibilityCheck: function(unityInstance, onsuccess, onerror) {
+                    onsuccess();
+                }
             }
         });
         
+        window.unityGame = unityInstance;
+
         if (window.PokiSDK) {
             window.PokiSDK.gameLoadingFinished();
             window.PokiSDK.gameplayStart();
@@ -38,5 +44,4 @@ loaderScript.onload = function() {
     }
 };
 
-// Insertar el script en el documento para iniciar la secuencia ordenada de descarga
 document.body.appendChild(loaderScript);
